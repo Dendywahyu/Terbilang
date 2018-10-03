1ccc364daf19f735b29179c5c63f999cfeb8f67c
Dendy Wahyu Prasetyo <dendy.prasetyo@students.amikom.ac.id>
2018-10-03 14:52:01 +0700

    Tugas2

---
+++ DemoWindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+
+namespace DemoWindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cek_Click(object sender, EventArgs e)
+        {
+            int buatvariabelbaru = int.Parse(textBox1.Text);
+            Terbilang Terbilang = new Terbilang();
+            listBox1.Items.Clear();
+            listBox1.Items.Add(Terbilang.TerbilangIndonesia(buatvariabelbaru));
+        }
+    }
+}
